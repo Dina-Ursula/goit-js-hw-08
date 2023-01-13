89bda79bcb5017ed9b6ad935e3c79229d6586ffb
Dina-Ursula <dinatyan26@gmail.com>
2023-01-13 23:12:54 +0200

    Added 02-video homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onTimeUpdate = function (data) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, `${data.seconds}`);
+};
+const throttledFunction = throttle(onTimeUpdate, 1000);
+player.on('timeupdate', throttledFunction);
+
+const currentTime = +localStorage.getItem(LOCAL_STORAGE_KEY);
+player.setCurrentTime(currentTime);
